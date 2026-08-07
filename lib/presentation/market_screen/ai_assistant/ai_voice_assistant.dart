de24218e2a3aaf93174626930a3b9a2b6344d541
Swati Singh <apnagodam@gmail.com>
2026-08-07 10:02:20 +0530

    fix: remove unused get.dart import and verify _detectedLanguageResult state definition

--- lib/presentation/market_screen/ai_assistant/ai_voice_assistant.dart
+++ lib/presentation/market_screen/ai_assistant/ai_voice_assistant.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
